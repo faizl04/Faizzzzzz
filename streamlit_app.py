30aba63247191faa28eddd1fd5db46f8246e5f45
faizl04 <mfl.lukmanulhakim@gmail.com>
2025-05-30 18:31:20 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,7 @@
 import streamlit as st
 
 st.title("🎈 welcome faiz grup")
-st.write(
-    "selamat datang di web saya."
-)
+st.write("selamat datang di web saya")
 st.title("YAH JUARA LAGI")
 st.header("ASA DINGIN GINI EY")
 st.image("Screenshot_20250528_201110_TikTok.jpg")
